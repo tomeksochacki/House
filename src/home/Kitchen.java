972cbac4790019552afbd7101ea172d40728e60e
tomek <tomeksochacki@wp.pl>
2020-03-30 23:22:35 +0200

    Initialize repository

---
+++ src/home/Kitchen.java
@@ -0,0 +1,8 @@
+package home;
+
+public class Kitchen {
+    public static void main(String[] args) {
+        Room room1 = new Room(4, 3,2);
+        
+    }
+}
---
+++ src/home/Room.java
@@ -0,0 +1,19 @@
+package home;
+
+public class Room {
+
+    public static final double quantity = 3;
+
+    protected int amountWindow;
+    protected int amountDoor;
+    protected int amountChair;
+
+    public Room(int aW, int aD, int aCh){
+        this.amountWindow = aW;
+        this.amountDoor = aD;
+        this.amountChair = aCh;
+    }
+
+}
+
+
